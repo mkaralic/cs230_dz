a5e30c08cc0b6396866e844d8b351fe6d9517b5e
Milorad Karalic <miki.karalic@gmail.com>
2024-10-14 20:34:33 +0200

    CS230_DZ01

--- CS230-DZ01/02/static.py
+++ CS230-DZ01/02/static.py
@@ -1,3 +1,9 @@
+# $env:FLASK_APP = "static.py"
+# $env:FLASK_DEBUG = 1
+# flask run
+# py -m flask run
+# http://127.0.0.1:5000/
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,3 +12,5 @@ app = Flask(__name__)
 def prva_strana():
     return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
